c72623fcb34020daba361a766b1c5fdaeffde9cd
David Lokshin <david@loksh.in>
2020-07-21 21:52:15 -0700

    deploy

---
+++ build/precache-manifest.7ae9e7c9d310ecdbd8d225d095c44a40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef46e4e392b25f3256f3c7a9c9a44a8",
+    "url": "/game-map/index.html"
+  },
+  {
+    "revision": "910f17fd66687749d78b",
+    "url": "/game-map/static/css/main.93b22a54.chunk.css"
+  },
+  {
+    "revision": "1ab6a81e372928a705e4",
+    "url": "/game-map/static/js/2.6836e363.chunk.js"
+  },
+  {
+    "revision": "1bcefd02b7d05499a818042383aa8e83",
+    "url": "/game-map/static/js/2.6836e363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910f17fd66687749d78b",
+    "url": "/game-map/static/js/main.fa1634d2.chunk.js"
+  },
+  {
+    "revision": "59b9c8b8485b3cce5095",
+    "url": "/game-map/static/js/runtime-main.79e55c5a.js"
+  }
+]);
